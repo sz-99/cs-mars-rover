ad50ebd58233ae2231bec42fc83877b03c352a32
sz-99 <zhengsusan3@gmail.com>
2024-12-05 16:34:08 +0000

    Add additional integration test for input and logic

--- mars-rover/mars-rover.Tests/IntegrationTests.cs
+++ mars-rover/mars-rover.Tests/IntegrationTests.cs
@@ -42,7 +42,41 @@ namespace mars_rover.Tests
             (rover1.CurrentPosition.y).Should().Be(3);
             (rover1.CurrentPosition.Facing).Should().Be(Direction.N);
 
+        }
 
+
+        [Test]
+        public void InputAndLogicIntegrationTest2()
+        {
+            //arrange
+            List<string> inputs = new List<string>()
+            {
+                "5 5",
+                "1 2 N",
+                "LMLMLMLMM",
+                "3 3 E",
+                "MMRMMRMRRM"
+            };
+
+            var mockInputProcessor = new Mock<IInputProcessor>();
+            mockInputProcessor.SetupSequence(input => input.ReadInput())
+                                                            .Returns(inputs[0])
+                                                            .Returns(inputs[3])
+                                                            .Returns(inputs[4]);
+
+            InputParser inputParser = new(mockInputProcessor.Object);
+
+            //act
+            PlateauSize plateauSize = inputParser.ParsePlateauInput();
+            Rover rover1 = inputParser.ParseLandingPosition();
+            List<Instruction> rover1Instructions = inputParser.ParseInstructions();
+
+            var result = rover1.Movement(rover1Instructions);
+
+            //assert
+            (rover1.CurrentPosition.x).Should().Be(5);
+            (rover1.CurrentPosition.y).Should().Be(1);
+            (rover1.CurrentPosition.Facing).Should().Be(Direction.E);
 
         }
 
