6130ff60764f0d55d201ee7bb92cd0f11d3b83dc
sz-99 <zhengsusan3@gmail.com>
2024-12-05 15:13:16 +0000

    Add Move method and tests in Rover class

--- mars-rover/mars-rover.Tests/LogicTests.cs
+++ mars-rover/mars-rover.Tests/LogicTests.cs
@@ -14,20 +14,38 @@ namespace mars_rover.Tests
         [TestCase(1, 2, Direction.N, Instruction.L, Direction.W)]
         [TestCase(5, 3, Direction.E, Instruction.R, Direction.S)]
         [TestCase(6, 6, Direction.E, Instruction.L, Direction.N)]
-
-
-
         public void RotateTest(int x, int y, Direction currentDirection, Instruction instruction, Direction expected)
         {
             //arrange
             Position position = new(x, y, currentDirection);
             Rover testRover = new Rover(position);
+            Position expectedResult = new Position(x, y, expected);
 
             //act
             var result = testRover.Rotate(instruction);
 
             //assert
-            result.Should().Be(expected);
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        [TestCase(1, 2, Direction.N, Instruction.M, 1, 3, Direction.N)]
+        [TestCase(5, 3, Direction.W, Instruction.M, 4, 3, Direction.W)]
+        [TestCase(3, 4, Direction.S, Instruction.M, 3, 3, Direction.S)]
+
+
+        public void MoveTest(int x, int y, Direction currentDirection, Instruction instruction, int newX, int newY, Direction newDirection)
+        {
+            //arrange
+            Position position = new(x, y, currentDirection);
+            Rover testRover = new Rover(position);
+            Position expectedResult = new(newX,newY, newDirection);
+
+            //act
+            var result = testRover.Move(instruction);
+
+            //assert
+            result.Should().BeEquivalentTo(expectedResult);
         }
 
     }
--- mars-rover/mars-rover/Rover.cs
+++ mars-rover/mars-rover/Rover.cs
@@ -16,7 +16,7 @@ namespace mars_rover
 
         public Position CurrentPosition {  get; set; }
 
-        public Direction Rotate(Instruction instruction)
+        public Position Rotate(Instruction instruction)
         {
             while (true)
             {
@@ -30,7 +30,7 @@ namespace mars_rover
                     {
                         CurrentPosition.Facing -= 1;
                     }
-                    return CurrentPosition.Facing;
+                    return CurrentPosition;
                 }
                 else if (instruction == Instruction.R)
                 {
@@ -42,11 +42,25 @@ namespace mars_rover
                     {
                         CurrentPosition.Facing += 1;
                     }
-                    return CurrentPosition.Facing;
+                    return CurrentPosition;
                 }
                 else { Console.WriteLine("Invalid Instruction, please try again."); }
                 
             }
+        }
+
+        public Position Move (Instruction instruction)
+        {
+            if (instruction == Instruction.M)
+            {
+                if (CurrentPosition.Facing == Direction.N) CurrentPosition.y += 1;
+                else if (CurrentPosition.Facing == Direction.E) CurrentPosition.x += 1;
+                else if (CurrentPosition.Facing == Direction.S) CurrentPosition.y -= 1;
+                else if(CurrentPosition.Facing == Direction.W) CurrentPosition.x -= 1;
+
+                return CurrentPosition;
+            }
+            else return CurrentPosition;
         }
 
     }
